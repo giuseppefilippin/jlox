3c7a0757612515f21532a35e450346d0d1396dfe
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-01-21 13:49:09 -0300

    bug fix

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 
 public class Lox {
@@ -40,7 +39,7 @@ public class Lox {
     }
   }
   private static void run(String source) {
-    com.craftinginterpreters.lox.Scanner scanner = new com.craftinginterpreters.lox.Scanner(source);
+    Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
     // For now, just print the tokens.
