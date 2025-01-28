bc368868dec9bb2543d08e675bfd1bb60c940419
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-01-28 14:22:59 -0300

    adding scope

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -36,7 +36,7 @@ class Parser {
       }
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
-    
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
       }
     private Stmt printStatement() {
@@ -60,6 +60,16 @@ class Parser {
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
     }
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+    
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+          statements.add(declaration());
+        }
+    
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+      }
     private Expr assignment() {
         Expr expr = equality();
     
