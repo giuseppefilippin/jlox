9f9e0a365933a262592513ecfd302226ab30062b
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-01-28 13:49:26 -0300

    pasing variables

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -16,7 +16,7 @@ class Parser {
     List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
     return statements; 
@@ -24,6 +24,16 @@ class Parser {
     private Expr expression() {
         return equality();
     }
+    private Stmt declaration() {
+        try {
+          if (match(VAR)) return varDeclaration();
+    
+          return statement();
+        } catch (ParseError error) {
+          synchronize();
+          return null;
+        }
+      }
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
     
@@ -34,6 +44,17 @@ class Parser {
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
     }
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect variable name.");
+    
+        Expr initializer = null;
+        if (match(EQUAL)) {
+          initializer = expression();
+        }
+    
+        consume(SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
+      }
     private Stmt expressionStatement() {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
@@ -97,7 +118,9 @@ class Parser {
         if (match(NUMBER, STRING)) {
         return new Expr.Literal(previous().literal);
         }
-
+        if (match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
+          }
         if (match(LEFT_PAREN)) {
         Expr expr = expression();
         consume(RIGHT_PAREN, "Expect ')' after expression.");
