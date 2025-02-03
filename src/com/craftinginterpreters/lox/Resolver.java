1a99ef4526df7568ae42ee6d4e9022fa39d3d402
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-03 16:31:46 -0300

    incrementing resolver class

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -7,8 +7,33 @@ import java.util.Stack;
 
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private final Interpreter interpreter;
+  private final Stack<Map<String, Boolean>> scopes = new Stack<>();
 
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
+  }
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    beginScope();
+    resolve(stmt.statements);
+    endScope();
+    return null;
+  }
+  void resolve(List<Stmt> statements) {
+    for (Stmt statement : statements) {
+      resolve(statement);
+    }
+  }
+  private void resolve(Stmt stmt) {
+    stmt.accept(this);
+  }
+  private void resolve(Expr expr) {
+    expr.accept(this);
+  }
+  private void beginScope() {
+    scopes.push(new HashMap<String, Boolean>());
+  }
+  private void endScope() {
+    scopes.pop();
   }
 }
