2cc56bb4e657ac76715e032fcf5c6574355aadea
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-13 13:01:08 -0300

    methods in classes

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -14,7 +14,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    METHOD
   }
   @Override
   public Void visitBlockStmt(Stmt.Block stmt) {
@@ -27,6 +28,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration); 
+    }
     return null;
   }
   @Override
