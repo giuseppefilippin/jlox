bcd5d107e04d63bcd64e85b9d6154b69437ee32a
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-02-17 11:06:53 -0300

    updating constructors and initializers

--- src/com/craftinginterpreters/lox/LoxFunction.java
+++ src/com/craftinginterpreters/lox/LoxFunction.java
@@ -5,14 +5,17 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
   private final Environment closure;
-  LoxFunction(Stmt.Function declaration, Environment closure) {
+  private boolean isInitializer;
+  LoxFunction(Stmt.Function declaration, Environment closure,
+              boolean isInitializer) {
+    this.isInitializer = isInitializer;
     this.closure = closure;
     this.declaration = declaration;
   }
   LoxFunction bind(LoxInstance instance) {
     Environment environment = new Environment(closure);
     environment.define("this", instance);
-    return new LoxFunction(declaration, environment);
+    return new LoxFunction(declaration, environment, isInitializer);
   }
   @Override
   public String toString() {
@@ -35,6 +38,7 @@ class LoxFunction implements LoxCallable {
     } catch (Return returnValue) {
       return returnValue.value;
     }
+    if (isInitializer) return closure.getAt(0, "this");
     return null;
   }
 }   
