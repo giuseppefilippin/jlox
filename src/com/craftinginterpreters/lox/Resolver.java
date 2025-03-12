4cdfc8b18c8abcc24741afbda4c4477cb612d4c6
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-03-12 16:35:54 -0300

    updating resolver.java

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -44,6 +44,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     if (stmt.superclass != null) {
       resolve(stmt.superclass);
     }
+    if (stmt.superclass != null) {
+      beginScope();
+      scopes.peek().put("super", true);
+    }
     beginScope();
     scopes.peek().put("this", true);
 
@@ -55,6 +59,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       resolveFunction(method, declaration); 
     }
     endScope();
+    if (stmt.superclass != null) endScope();
     currentClass = enclosingClass;
     return null;
   }
@@ -159,6 +164,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitSetExpr(Expr.Set expr) {
     resolve(expr.value);
     resolve(expr.object);
+    return null;
+  }
+  @Override
+  public Void visitSuperExpr(Expr.Super expr) {
+    resolveLocal(expr, expr.keyword);
     return null;
   }
   @Override
